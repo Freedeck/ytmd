ee659118425bf497e7d5d57b783bf507788d4b9b
Roizor <aidenjs9@icloud.com>
2023-12-10 10:39:24 -0600

    update some stuff to reflect Freedeck's runtime

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 const AsarBundleRunner = require('asar-bundle-runner');
 const asar = require('@electron/asar');
+const { cpSync } = require('node:fs');
+const path = require('node:path');
 /**
  * * Freedeck Developer Environment
  * Welcome!
  * - Edit the FDE_Settings object below to configure your environment.
  */
 
-let name = 'ExamplePlugin'
+let name = 'BytenodeTest'
 let bundleName = name + '.Freedeck'
 // let bundleName = 'ExamplePlugin.Freedeck'
 const FDE_Settings = {
@@ -14,9 +16,6 @@ const FDE_Settings = {
 	BundlePrebuild: name,
 	BundleName: bundleName,
 	scripts: [],
-	build: [buildPhase],
-	extract: [extractPhase],
-	run: [runPhase],
 	_abr_extract: "",
 	_abr_run_output: ""
 }
@@ -25,33 +24,16 @@ function buildPhase() {
 	return new Promise((resolve, reject) => {
 		console.log('Building!')
 		asar.createPackage(FDE_Settings.BundlePrebuild + ".src", FDE_Settings.BundleName).then(() => {
-			console.log('Created bundle: ' + FDE_Settings.BundleName)
-			extractPhase().then(() => {
-			}).catch(console.error)
-			resolve(true);
+			cpSync(path.resolve(FDE_Settings.BundleName), path.resolve('./plugins/' + FDE_Settings.BundleName))
+
+			AsarBundleRunner.extract("./plugins/"+FDE_Settings.BundleName).then(bundleName => {
+				AsarBundleRunner.run(bundleName).then(output => {
+					FDE_Settings._abr_run_output = output
+					resolve(true)
+				}).catch(reject)
+
+			}).catch(reject)
 		}).catch(console.error)
-	})
-}
-
-function extractPhase() {
-	return new Promise((resolve, reject) => {
-		AsarBundleRunner.extract(require('node:path').join(FDE_Settings.BundleName)).then(bundleName => {
-			FDE_Settings._abr_extract = bundleName
-			console.log('Extracted bundle: ' + bundleName + ' to ' + FDE_Settings._abr_extract)
-			runPhase().then(() => {
-				resolve(true);
-			}).catch(console.error)
-			resolve(true);
-		}).catch(reject)
-	})
-}
-
-function runPhase() {
-	return new Promise((resolve, reject) => {
-		AsarBundleRunner.run(FDE_Settings._abr_extract).then(output => {
-			FDE_Settings._abr_run_output = output
-			resolve(true)
-		}).catch(reject)
 	})
 }
 
