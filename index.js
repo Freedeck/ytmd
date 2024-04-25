b325cca2e17f0206815d1cbe684530cfd5f11c3d
Aiden <aidenjs9@icloud.com>
2024-04-25 11:08:36 -0500

    make dev  env better

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const AsarBundleRunner = require('asar-bundle-runner');
 const asar = require('@electron/asar');
 const { cpSync } = require('node:fs');
 const path = require('node:path');
+const { exec } = require('node:child_process');
 /**
  * * Freedeck Developer Environment
  * Welcome!
@@ -12,6 +13,7 @@ let name = 'ExampleJSHook'
 let bundleName = name + '.Freedeck'
 // let bundleName = 'ExamplePlugin.Freedeck'
 const FDE_Settings = {
+	test: true,
 	UseScripts: true,
 	BundlePrebuild: name,
 	BundleName: bundleName,
@@ -24,15 +26,24 @@ function buildPhase() {
 	return new Promise((resolve, reject) => {
 		console.log('Building!')
 		asar.createPackage(FDE_Settings.BundlePrebuild + ".src",  path.resolve('./plugins/' + FDE_Settings.BundleName)).then(() => {
+			console.log('Created ASAR package.')
 			cpSync(path.resolve('./plugins',FDE_Settings.BundleName), path.resolve('./build/' + FDE_Settings.BundleName))
-
-			AsarBundleRunner.extract("./plugins/"+FDE_Settings.BundleName).then(bundleName => {
-				AsarBundleRunner.run(bundleName).then(output => {
-					FDE_Settings._abr_run_output = output
-					resolve(true)
+			console.log('Copied to plugins folder. Build completed.')
+			if(!FDE_Settings.test) {console.log('No tests to do! Finished.'); return;}
+			exec('npm i',{
+				cwd: path.resolve(FDE_Settings.BundlePrebuild + ".src")
+			}, (err, stdo, stde) => {
+				console.log('Installed dependencies for package for testing')
+				AsarBundleRunner.extract("./plugins/"+FDE_Settings.BundleName).then(bundleName => {
+					console.log('Simulated extraction with ABR')
+					AsarBundleRunner.run(bundleName).then(output => {
+						console.log('Simulated initialization with ABR')
+						FDE_Settings._abr_run_output = output
+						resolve(true)
+					}).catch(reject)
+	
 				}).catch(reject)
-
-			}).catch(reject)
+			})
 		}).catch(console.error)
 	})
 }
